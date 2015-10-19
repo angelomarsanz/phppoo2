48e36c91f7856a260863e12b5690e96c1505e2e9
angelomarsanz <angelomarsanz@gmail.com>
2015-10-19 04:33:13 +0000

    Versión 2015-10-18

--- clase3.php
+++ clase3.php
@@ -1,11 +1,10 @@
 <?php
 include "ClaseSencilla.php";
-include "Foo.php";
 
 $instancia = new ClaseSencilla();
 
 // Esto también se puede hacer con una variable:
 
-$nombreClase = 'Foo';
+$nombreClase = 'ClaseSencilla';
 $instancia = new $nombreClase(); // Foo()
 ?>
